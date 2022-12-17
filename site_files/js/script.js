c9ce4d141968845206a15fcef224f986e30f5e5f
nicksands1 <nicksanders41@gmail.com>
2022-12-17 11:49:42 -1000

    updated service and typing text

--- site_files/js/script.js
+++ site_files/js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    strings:["","Cloud Engineer","Student","Greek Yogurt Connoisseur"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
